1ad1661b9580b84c569279a17883db5c9852e585
PiyushMaharjan <maharjanpiyush90@gmail.com>
2026-06-23 12:49:25 +0545

    this is my first program in python

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def main():
+    x,y=input("enter two numbers").split()
+    sum(x,y)
+    diff(x,y)
+    multiply(x,y)
+    divison(x,y)
+
+def sum(x,y):
+    sum=int(x)+int(y)
+    print(f"the sum is,:,{sum}")
+
+def diff(x,y):
+    diff=int(x)-int(y)
+    print(f"the diff is,:,{diff}")
+
+def multiply(x,y):
+    mul=int(x)*int(y)
+    print(f"the multiplication is,:,{mul}")
+
+def divison(x,y):
+    div=int(x)/int(y)
+    print(f"the division is,:,{div}")
+
+main()
---
+++ python.py
@@ -0,0 +1,8 @@
+def hello(to= "pasa"):
+    print("hello,",to )
+
+
+hello()
+name = input("how are you today?")
+print(name)
+
